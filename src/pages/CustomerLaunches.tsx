c990f8d3709446376d5ae661b0e53a8c8afd96d5
Saurav Khare <saurav.erahk@gmail.com>
2023-09-03 00:34:29 +0530

    padding change

--- src/pages/CustomerLaunches.tsx
+++ src/pages/CustomerLaunches.tsx
@@ -16,7 +16,7 @@ function CustomerLaunches() {
 
   return (
     <section className="bg-zinc-900 min-h-screen text-white">
-      <div className="px-40">
+      <div className="px-40 pt-10">
         <h1 className="font-bold text-5xl">Customer Launches</h1>
         <p className="text-sm mt-4">
           All the spacecrafts, satellite launched by ISRO for other countries &
